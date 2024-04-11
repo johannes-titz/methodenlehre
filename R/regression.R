73555e4412cc6b4f1bcc7d58259295af05db126b
Johannes Titz <johannes@titz.science>
2024-04-11 12:44:03 +0200

    feat: add alle elements of regression
    - table
    - figure
    - fix html entities
    - feedback
    - remove teaching data set for now

--- R/regression.R
+++ R/regression.R
@@ -1,7 +1,7 @@
 
 #' helper function to extract parameters for regression exercise from lm model
 #' @parameter model lm model object
-#' @return list of important parameters, rounded to 3 decimal points 
+#' @return list of important parameters, rounded to 3 decimal points
 extract_parameters <- function(model) {
   data <- model$model
   fit <- lm.beta::lm.beta(model, complete.standardization = T)
@@ -41,7 +41,7 @@ prepare_tovee <- function() {
   which_rows <- sample(1:nrow(d), size = sample(35:45, 1),
                        replace = T)
   data <- d[which_rows, 2:1]
-  story <- paste0("In einer bekannten Studie untersuchten Tovée et al. (1999) wie weibliche Studierende die Attraktivität von Männern mit unterschiedlichen Körpermaßen bewerten. In der Studie kam heraus, dass der Waist-to-Chest-Ratio <i>wcr</i>, der beste Prädiktor für die Attraktivitätsbewertung <i>attract</i> ist. Der Waist-to-Chest-Ratio ist das Verhältnis zwischen Hüfte und Schultern (hier ausgedrückt in %). Sie finden einen Teil des Datensatzes im Internet, bestehend aus ", nrow(data), " bewerteten Männern.")
+  story <- paste0("In einer bekannten Studie untersuchten Tovée et al. (1999) wie weibliche Studierende die Attraktivität von Männern mit unterschiedlichen Körpermaßen bewerten. In der Studie kam heraus, dass der Waist-to-Chest-Ratio <em>wcr</em>, der beste Prädiktor für die Attraktivitätsbewertung <em>attract</em> ist. Der Waist-to-Chest-Ratio ist das Verhältnis zwischen Hüfte und Schultern (hier ausgedrückt in %). Sie finden einen Teil des Datensatzes im Internet, bestehend aus ", nrow(data), " bewerteten Männern.")
   tibble::lst(data, story)
 }
 
@@ -53,13 +53,13 @@ prepare_prestige <- function() {
   which_rows <- sample(1:nrow(Prestige), size = sample(70:95, 1),
                        replace = FALSE)
   data <- Prestige[which_rows, sample(1:4, 2)]
-  story <- paste0("Der <i>Prestige</i> Datensatz besteht aus 102 Berufen/Berufsgruppen eines Zensus in Kanada von 1971. Die Variablen sind unter anderem der durchschnittliche Bildungsgrad in Jahren (<i>education</i>), das durchschnittliche Einkommen in der Einheit 1000 USD pro Jahr (<i>income</i>), der Anteil an Frauen in % (<i>women%</i>) sowie der Pineo-Porter Prestige Score (<i>prestige</i>), der das Ansehen eines Berufs erfasst. Die Variablen beziehen sich alle auf die Berufsgruppe. ",
+  story <- paste0("Der <em>Prestige</em> Datensatz besteht aus 102 Berufen/Berufsgruppen eines Zensus in Kanada von 1971. Die Variablen sind unter anderem der durchschnittliche Bildungsgrad in Jahren (<em>education</em>), das durchschnittliche Einkommen in der Einheit 1000 USD pro Jahr (<em>income</em>), der Anteil an Frauen in % (<em>women%</em>) sowie der Pineo-Porter Prestige Score (<em>prestige</em>), der das Ansehen eines Berufs erfasst. Die Variablen beziehen sich alle auf die Berufsgruppe. ",
                  "Sie finden den Datensatz im Internet, jedoch ist dieser unvollständig mit nur ", nrow(data), " Berufen.\n")
   tibble::lst(data, story)
 }
 
 prepare_teaching_ratings <- function() {
-  TeachingsRatnges <- AER::TeachingRatings
+  TeachingsRatings <- get_data("TeachingRatings")
 
   N <- nrow(TeachingRatings)
   which_rows <- sample(1:nrow(TeachingRatings), size = sample(round(N*0.9):N, 1),
@@ -80,7 +80,7 @@ prepare_davis <- function() {
                        replace = FALSE)
   data <- Davis[which_rows, sample(2:5, 2)]
 
-  story <- paste0("Davis (1990) führte eine Studie zur Körperwahrnehmung und Gewicht durch (N=200). Er verglich Personen die Sport trieben, mit denen die keinen Sport trieben. Unter anderem erfasste Davis das Körpergewicht (<i>weight</i>) und die Körpergröße (<i>height</i>), sowie das wahrgenommene Körpergewicht (<i>repwt</i>) und die wahrgenommene Körpergröße (<i>repht</i>). ",
+  story <- paste0("Davis (1990) führte eine Studie zur Körperwahrnehmung und Gewicht durch (N=200). Er verglich Personen die Sport trieben, mit denen die keinen Sport trieben. Unter anderem erfasste Davis das Körpergewicht (<em>weight</em>) und die Körpergröße (<em>height</em>), sowie das wahrgenommene Körpergewicht (<em>repwt</em>) und die wahrgenommene Körpergröße (<em>repht</em>). ",
                  "In einer Replikationsstudie wurde die gleiche Untersuchung nochmals durchgeführt, diesmal mit ", nrow(data), " Probanden.")
   tibble::lst(data, story)
 }
@@ -91,12 +91,13 @@ prepare_sleepstudy <- function() {
   sleepstudy$Days <- sleepstudy$Days - 2
   data <- sleepstudy[sleepstudy$Subject %in% which & sleepstudy$Days >= 0, c("Reaction", "Days")]
   data$Reaction <- round(data$Reaction)
-  story <- paste0("Belenky et al. (2003) führten eine Studie zu Schlafdeprivation durch und erfassten für 18 Probanden die Reaktionszeit in ms (<i>Reaction</i>) über mehrere Tage (<i>Days</i>). Tag 0 zählt hierbei als Baseline (es gab keine Schlafdeprivation). Sie schauen sich einen Teil des Datensatzes an mit insgesamt ", nrow(data), " Messungen von ", length(which), " Probanden.")
+  story <- paste0("Belenky et al. (2003) führten eine Studie zu Schlafdeprivation durch und erfassten für 18 Probanden die Reaktionszeit in ms (<em>Reaction</em>) über mehrere Tage (<em>Days</em>). Tag 0 zählt hierbei als Baseline (es gab keine Schlafdeprivation). Sie schauen sich einen Teil des Datensatzes an mit insgesamt ", nrow(data), " Messungen von ", length(which), " Probanden.")
   tibble::lst(data, story)
 }
 
 prep_functions <- function() {
-  c("prepare_tovee", "prepare_sleepstudy", "prepare_davis", "prepare_prestige", "prepare_teaching_ratings")
+  c("prepare_tovee", "prepare_sleepstudy", "prepare_davis", "prepare_prestige")
+  #, "prepare_teaching_ratings")
 }
 
 #' takes prepare function and produces a list with all variables needed
@@ -108,7 +109,7 @@ regression_example <- function(prepare_function, seed) {
     story <- example$story
 
     variables <- names(data)
-    variables_md <- paste0("<i>", names(data), "</i>")
+    variables_md <- paste0("<em>", names(data), "</em>")
     model <- lm(data)
     model_sd <- lm.beta::lm.beta(model)
 
@@ -122,24 +123,30 @@ regression_example <- function(prepare_function, seed) {
 #' writes out sjPlot regression table to table.html
 #' @sem standard error of measurement, which is not available in model #' and not generated by tab_model
 get_html_table <- function(model, sem) {
-     sjPlot::tab_model(
-         model,
-         show.ci = F,
-         show.se = T,
-         show.stat = T,
-         show.p = T,
-         show.fstat = T, show.df = F, show.r2 = F, digits.p = 3,
-         show.std = T, string.pred = "Predictor", string.est = "b",
-         string.se = "SE (b)", string.std = "beta",
-         string.std_se = "SE (beta)", string.stat = "t-Wert",
-         emph.p = F,
-         show.aic = sample(c(T, F), 1),
-         digits = 3,
-         show.dev = sample(c(T, F), 1),
-         show.loglik = sample(c(T, F), 1),
-         title  = paste0("Residual Standard Error (Standardschätzfehler) = ", sem),
-         file = "table.html"
-     )
+  filepath <- tempfile()
+  # use print method, otherwise print.sjTable ist not called
+  print(sjPlot::tab_model(
+    model,
+    show.ci = F,
+    show.se = T,
+    show.stat = T,
+    show.p = T,
+    show.fstat = T, show.df = F, show.r2 = F, digits.p = 3,
+    show.std = T, string.pred = "Predictor", string.est = "b",
+    string.se = "SE (b)", string.std = "beta",
+    string.std_se = "SE (beta)", string.stat = "t-Wert",
+    emph.p = F,
+    show.aic = sample(c(T, F), 1),
+    digits = 3,
+    show.dev = sample(c(T, F), 1),
+    show.loglik = sample(c(T, F), 1),
+    title  = paste0("Residual Standard Error (Standardschätzfehler) = ", sem),
+    file = filepath
+  ))
+  lines <- paste0(readLines(filepath), collapse = "")
+  lines <- gsub("&lt;", "&#60;", lines)
+  lines <- gsub("&gt;", "&#62;", lines)
+  textutils::HTMLdecode(lines, hex = F, decimal = F)
 }
 
 prepare_html <- function(obj) {
@@ -155,11 +162,8 @@ prepare_html <- function(obj) {
         theme_classic()
     html_plot <- encodeGraphic(reg_plot)
     tab <- get_html_table(obj$model, obj$parameters$sem)
-    print(tab, file = "table.html")
-
-    story_part2 <- "Beantworten Sie folgende Fragen. Wenn nötig, runden Sie bei numerischer Eingabe auf <b>3 Dezimalstellen</b>." 
-    tbl <- paste(readLines("table.html"), collapse = "")
-    c(obj$story, html_plot, tbl, story_part2)
+    story_part2 <- "Beantworten Sie folgende Fragen. Wenn nötig, runden Sie bei numerischer Eingabe auf <b>3 Dezimalstellen</b>."
+    c(obj$story, html_plot, tab, story_part2)
 }
 
 #' encode ggplot graphic as png and return as html string <img> in base64
@@ -170,7 +174,7 @@ encodeGraphic <- function(g) {
   print(g)  # Output a graphic to the file
   dev.off()  # Close the file.
   txt <- RCurl::base64Encode(readBin(tf1, "raw", file.info(tf1)[1, "size"]), "txt")  # Convert the graphic image to a base 64 encoded string.
-  myImage <- htmltools::HTML(paste0('<img src="data:image/png;base64,', txt, '">'))
+  myImage <- htmltools::HTML(paste0('<img src="data:image/png;base64,', txt, '"/>'))
   # Save the image as a markdown-friendly html object.
   return(myImage)
 }
@@ -296,11 +300,16 @@ get_questions <- function(parameters, variables_md) {
 }
 
 regression <- function() {
-  tovee1 <- regression_example(sample(prep_functions(), 1), seed = sample.int(1e6, 1))
-  content <- prepare_html(tovee1)
-  q <- get_questions(tovee1$parameters, tovee1$variables_md)
+  ex <- regression_example(sample(prep_functions(), 1),
+                               seed = sample.int(1e6, 1))
+  content <- prepare_html(ex)
+  q <- get_questions(ex$parameters, ex$variables_md)
   questions <- df2gap(q)
-  cont <- append(content[1], questions)
-  render_qtijs(new("Entry", content = cont))
+  feedback <- new("ModalFeedback", title = "Feedback",
+                  content = list(paste("<p><details><summary>", q$question, "</summary>",
+                                       textutils::HTMLdecode(q$feedback), "</details></p>")))
+  cont <- append(content, questions)
+  new("Entry", identifier = "regression", content = cont,
+      feedback = list(feedback))
 }
 
