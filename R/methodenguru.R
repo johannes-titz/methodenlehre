504aa57d024c8b170deb8918f205f7ffbea7a6a8
Johannes Titz <johannes@titz.science>
2024-05-31 11:04:25 +0200

    build: export all methodenguru functions for use in analyses

--- R/methodenguru.R
+++ R/methodenguru.R
@@ -1,26 +1,35 @@
-
+#' @export
 methodenguru_resource_id <- function() {
   return("1671679683120887006")
 }
 
+#' @export
 methodenguru_course_id <- function() {
   "38156107780"
 }
 
+#' @export
 methodenguru_node_id <- function(shortName) {
   d <- get_course_elements(methodenguru_resource_id())
   d$nodeId[d$shortName == shortName]
 }
 
+#' Data from opal methodenguru course
+#'
+#' @param shortName Short name of exercise.
+#' @path where to download to
+#' @export
 methodenguru_data <- function(shortName, path = "data-raw/results/") {
   node_id <- methodenguru_node_id(shortName)
   get_course_results(methodenguru_resource_id(), node_id, path = path)
 }
 
+#' @export
 methodenguru_elements <- function() {
   get_course_elements(methodenguru_resource_id())
 }
 
+#' @export
 methodenguru_url <- function(shortName) {
   node_id <- methodenguru_node_id(shortName)
   course_id <- methodenguru_course_id()
