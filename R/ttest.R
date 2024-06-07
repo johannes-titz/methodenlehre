a0ec4ee9784b063a15c8581a487df7f68b99800f
Johannes Titz <johannes@titz.science>
2024-06-07 10:47:00 +0200

    refactor: ttest
    - individual question functions
    - finish story, questions
    - fix hypothesis
    - use group labels for countries
    - set tolerance

--- R/ttest.R
+++ R/ttest.R
@@ -17,13 +17,15 @@ ttest_data <- function() {
   hypothesis <- c("two.sided" = mathml(quote(mu["A"] != mu["B"])),
                   "less" = mathml(quote(mu["A"] < mu["B"])),
                   "greater" = mathml(quote(mu["A"] > mu["B"])))
+  hypothesis <- html_to_xml(hypothesis)
   actual_alphas <- c(alpha / 2, alpha, 1 - alpha)
   df <- nrow(data) - 2
   critical_t <- lapply(actual_alphas, function(alpha) mml_eq(qt(alpha, df), T))
   critical_t <- plyr::ldply(critical_t, data.frame)
   df_results <- data.frame(hypothesis = names(hypothesis), mml = hypothesis,
-                           alpha = actual_alphas, critical_t = round(critical_t$res, 3),
-    mml_t = critical_t$mml)
+                           alpha = actual_alphas,
+                           critical_t = round(critical_t$res, 3),
+                           mml_t = critical_t$mml)
 
   result <- df_results %>%
     dplyr::sample_n(1)
@@ -32,42 +34,97 @@ ttest_data <- function() {
                    '1="very accurate" bis 5="very inaccurate"')
 
   ttest <- t.test(dv ~ country, data = data, var.equal = T,
-                  alternative = names(hypothesis), alpha = alpha)
+                  alternative = result$hypothesis, alpha = alpha)
 
   ttest_output <- capture.output(print(ttest))
-  ttest_output <- paste("<pre>", paste(ttest_output, collapse = "<br/>"), "</pre>", collapse = "")
+  ttest_output <- gsub("<", "&#60;", ttest_output)
+  ttest_output <- paste("<pre>", paste(ttest_output, collapse = "<br/>"),
+                        "</pre>", collapse = "")
 
   #es <- effectsize::cohens_d(ttest)
   n <- as.numeric(table(data$country))
   t <- ttest$statistic
-
-  tibble::lst(n, t, ttest_output, labels, countries, result, df, alpha)
+  p <- ttest$p.value
+  tibble::lst(n, t, ttest_output, labels, countries, result, df, alpha, p,
+              item)
 }
 
-ttest <- function() {
-  ttest_data <- ttest_data()
+ttest_d <- function(ttest_data) {
   t <- ttest_data$t
   n <- ttest_data$n
   df <- ttest_data$df
-  mml_d <- mml_eq(d <- t * dfrac((n[1] + n[2]), (sqrt(df * n[1] * n[2]))), T)
-  mml_n <- mml_eq(n[2] <- df + 2 - n[1], T)
-
-  #solution <- c(ttest_data$result$critical_t)
-  #              as.numeric(ttest$p.value < alpha),
-  #              mround(mml_d$res, 3),
-ml#_n$res,
-
-  #  fb2 <-
-
-  #fb <- c(part1, mml_d$mml, "Hier müssen wir nur schauen ob der p-Wert kleiner oder gleich Alpha ist.", mml_n$mml, result$mml_t)
-  #extol <- c(abs(round(solutions[1], 3)*0.01), 0, 0, 0.041)
-  #tolerance <- c(0.01, 0, 0, 0.041)
-  #qdf <- data.frame(question, solution, tolerance, tolerance_type = "absolute")
+  mml_d <- mml_eq(d <- t * dfrac(n[1L] + n[2L], sqrt(df * n[1L] * n[2L])), T)
+  solution <- mml_d$res
+  q <- "Wie groß ist die Effektgröße d?"
+  fb <- glue::glue("Die Effektgröße für unabhängige Stichproben kann man direkt aus dem t-Wert approximieren: {mml_d$mml}. Eine Stichprobengröße steht schon da, die andere lässt sich aus den Freiheitsgraden berechnen (siehe Frage 3). Bitte beachten Sie, dass diese Effektgröße nur eine Approximation ist. Um den exakten Wert auszurechnen, brauchen Sie die Rohdaten.")
+  list(q = list("<p>", q, numericGap(solution, "d", tolerance = 1,
+                                    tolerance_type = "relative"), "</p>"),
+       fb = hug_fb(fb, q), solution = solution)
 }
 
-ttest_story <- function(item_name, hypothesis, labels) {
+ttest_sig <- function(ttest_data) {
+  alpha <- ttest_data$alpha
+  q <- glue::glue("Ist das Ergebnis bei einem Alpha von {alpha} signfikant? (0 = nicht signfikant, 1 = signifikant)")
+  p <- ttest_data$p
+  solution <- as.numeric(p <= alpha)
+  fb <- glue::glue("Das Ergebnis ist signifikant wenn {M(p<=alpha)}. Die korrekte Angabe wäre hier also {solution}.")
+  list(q = list("<p>", q, numericGap(solution, "sig", points = 0.5), "</p>"),
+       fb = hug_fb(fb, q), solution = solution)
+}
+
+ttest_krit <- function(ttest_data) {
+  alpha <- ttest_data$alpha
+  q <- glue::glue("Was ist der kritische t-Wert bei einem Alpha von {alpha}. Wenn sie mehrere t-Werte angeben möchten, geben Sie nur den kleineren an.")
+  df <- ttest_data$df
+  fb <- fb_critical_t(ttest_data$result$hypothesis, ttest_data$result$mml,
+                      ttest_data$result$critical_t,
+                      ttest_data$alpha, df, ttest_data$result$mml_t)
+  solution <- ttest_data$result$critical_t
+  list(q = list("<p>", q, numericGap(solution, "crit", tolerance = 0.041,
+                                    tolerance_type = "absolute"), "</p>"),
+       fb = hug_fb(fb, q), solution = solution)
+}
+
+ttest_n <- function(ttest_data) {
+  n <- ttest_data$n
+  df <- ttest_data$df
+  mml_n <- mml_eq(n[2L] <- df + 2L - n[1L], T)
+  q <- "Wie groß ist die Gesamtstichprobe?"
+  fb <- glue::glue("Die Gesamtstichprobe kann aus den Freiheitsgraden berechnet werden. Für unabhängige Stichproben gilt bei Varianzhomogenität: {mml_n$mml}.")
+  solution <- mml_n$res
+  list(q = list("<p>", q, numericGap(solution, "n"), "</p>"),
+       fb = hug_fb(fb, q),
+       solution = solution)
+}
+
+ttest_one <- function(seed = sample.int(1e4, 1)) {
+  set.seed(seed)
+  ttest_data <- ttest_data()
+  ttest_story <- ttest_story(ttest_data$item$item_char,
+                             ttest_data$item$item_content,
+                             ttest_data$result$mml,
+                             ttest_data$labels, ttest_data$n,
+                             ttest_data$countries,
+                             ttest_data$ttest_output)
+  q1 <- ttest_d(ttest_data)
+  q2 <- ttest_sig(ttest_data)
+  q3 <- ttest_n(ttest_data)
+  q4 <- ttest_krit(ttest_data)
+  q <- list(q1, q2, q3, q4)
+  questions <- sapply(q, function(x) x$q)
+  feedback <- unlist(lapply(q, function(x) textutils::HTMLdecode(x$fb)))
+
+  content <- list(ttest_story)
+  content <- append(content, questions)
+  fb <- modalFeedback(list(feedback))
+  new("Entry", identifier = paste0("ttest", seed),
+      content = unlist(content), feedback = list(fb))
+}
+
+ttest_story <- function(item_name, item_content, hypothesis, labels, n,
+                        countries, ttest_output) {
   glue::glue("
-<p>Johnson (2014) entwickelte einen offenen Big-Five-Fragebogen anhand eines großen Internet-Samples. Sie re-analysieren die Daten und schauen sich Unterschiede zwischen verschiedenen Ländern an. In einer Analyse vergleichen Sie das Item {item_name} (<i>I {item_content}</i>) mit den Ausprägungen von {labels} zwischen Teilnehmern aus {countries[1]} (n = {n[1]} und {countries[2]} (n nicht angegeben). Hierfür rechnen Sie einen t-Test mit der Hypothese H0: {hypothesis}
+<p>Johnson (2014) entwickelte einen offenen Big-Five-Fragebogen anhand eines großen Internet-Samples. Sie reanalysieren die Daten und schauen sich Unterschiede zwischen verschiedenen Ländern an. In einer Analyse vergleichen Sie das Item {item_name} (<i>I {item_content}</i>) mit den Ausprägungen von {labels} zwischen Teilnehmern aus Gruppe A: {countries[1]} (n = {n[1]}) und Gruppe B: {countries[2]} (n nicht angegeben). Hierfür rechnen Sie einen t-Test mit der Hypothese H1: {hypothesis}
 </p>
 <p>
 {ttest_output}
@@ -77,18 +134,6 @@ Beantworten Sie folgende Fragen. Runden Sie, wenn nötig, auf 3 Dezimalstellen.
 </p>")
 }
 
-#ttest_questions <- function() {
-#  * Wie groß ist die Effektgröße $d$? (1 Punkt) `r gap_numeric(solutions[1], tolerance = 0.01)`
-#  * Ist das Ergebnis bei einem Alpha von `r alpha` signifikant? (0 = nicht signifikant, 1 = signifikant) (0.5 Punkte) `r gap_numeric(solutions[2])`
-#  * Wie groß ist die Gesamt-Stichprobe? (1 Punkt) `r gap_numeric(solutions[3])` <!-- sollte raus, da statistiken schlecht-->
-#  * Was ist der kritische $t$-Wert bei einem Alpha von `r alpha`. Wenn sie mehrere t-Werte angeben möchten, geben Sie nur den kleineren an. (1 Punkt) `r gap_numeric(solutions[4], tolerance = 0.041)`
-#}
-
-#feedback <- c("Die Effektgröße für unabhängige Stichproben kann man direkt aus dem t-Wert approximieren: $d = t\\cdot\\frac{n_A+n_B}{\\sqrt{\\mathrm{df}\\cdot n_A\\cdot n_B}}$. Eine Stichprobengröße steht schon da, die andere lässt sich aus den Freiheitsgraden berechnen (siehe Frage 3). Bitte beachten Sie, dass diese Effektgröße nur eine Approximation ist. Um den exakten Wert auszurechnen, brauchen Sie die Rohdaten.",
-#"Das Ergebnis ist signifikant wenn $p\\leq\\alpha$.",
-#"Die Gesamtstichprobe kann aus den Freiheitsgraden berechnet werden. Für unabhängige Stichproben gilt bei Varianzhomogenität: $\\mathrm{df} = n_A-1+n_B-1 = N-2$. Die Formel nach N umstellen und schon haben wir die Gesamtstichprobengröße.",
-#aussage)
-
 fb_critical_t <- function(hypothesis, hypothesis_mml, critical_t, alpha, df, mml_t) {
   df120 <- ifelse(df > 120, glue::glue("Da die Tabelle bei {mathml(quote(df==120L))} endet, gehen wir approximativ von unendlich großen Freiheitsgraden aus."), "")
   part1 <- glue::glue("<p>Um den kritischen t-Wert zu ermitteln brauchen wir die Freiheitsgrade, die im Output angegeben sind: {mml_eq(df)}. {df120} </p> <p>Der kritische t-Wert hängt von der Alternativhypothese ab. In diesem Fall ist die Hypothese: {hypothesis_mml}.")
@@ -97,7 +142,7 @@ fb_critical_t <- function(hypothesis, hypothesis_mml, critical_t, alpha, df, mml
 
   aussage2 <- glue::glue("{part1} Stellen wir uns die t-Verteilung vor, erwarten wir also ein extremes Ergebnis auf der linken Seite der Verteilung. Alpha muss in diesem Fall nicht halbiert werden und es gibt nur einen kritischen t-Wert für die Fläche von {alpha}. Da Tabellen üblicherweise die Integrale für große Flächenanteile angeben können wir den t-Wert nicht direkt ablesen. Die t-Verteilung ist allerdings symmetrisch also schauen wir einfach bei {1-alpha} und finden einen t-Wert von {-critical_t}, den wir nur noch negieren müssen: {critical_t}.</p>")
 
-  aussage3 <- glue::glue("{part1} Stellen wir uns die t-Verteilung vor, erwarten wir also ein extremes Ergebnis auf der rechten Seite der Verteilung. Alpha muss in diesem Fall nicht halbiert werden und es gibt nur einen kritischen t-Wert für die Fläche {1 - alpha}. Da Tabellen üblicherweise die Integrale für große Flächenanteile angeben, können wir den t-Wert direkt bei {1-alpha} ablesen und finden einen t-Wert von {critical_t}.")
+  aussage3 <- glue::glue("{part1} Stellen wir uns die t-Verteilung vor, erwarten wir also ein extremes Ergebnis auf der rechten Seite der Verteilung. Alpha muss in diesem Fall nicht halbiert werden und es gibt nur einen kritischen t-Wert für die Fläche {1 - alpha}. Da Tabellen üblicherweise die Integrale für große Flächenanteile angeben, können wir den t-Wert direkt bei {1-alpha} ablesen und finden einen t-Wert von {critical_t}.</p>")
 
   aussagen <- c(two.sided = aussage1, less = aussage2, greater = aussage3)
   aussage <- aussagen[hypothesis]
