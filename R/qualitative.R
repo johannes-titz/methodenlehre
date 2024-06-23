a55d46786dea53959efc4aa5afa9aa8f1fa2d8c0
Johannes Titz <johannes@titz.science>
2024-06-23 20:37:56 +0200

    docs: update roxygen

--- R/qualitative.R
+++ R/qualitative.R
@@ -10,6 +10,8 @@ qualitative_aussagen <- function() {
   list(p1, p2, p3, p4)
 }
 
+#' Exercise qualitative
+#' @export
 qualitative <- function(which_questions = sample(1:4, 1)) {
   qa <- qualitative_aussagen()
   qa2 <- unlist(qa[which_questions])
