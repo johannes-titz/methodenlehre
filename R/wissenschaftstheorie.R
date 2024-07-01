f4b2dcfa0dfc87dae439eb6faf7d820c80bd6c67
Johannes Titz <johannes@titz.science>
2024-07-01 12:44:21 +0200

    feat: add story to wt

--- R/wissenschaftstheorie.R
+++ R/wissenschaftstheorie.R
@@ -6,10 +6,15 @@
 #' @param seed seed for exercise
 #' @export
 wissenschaftstheorie <- function(seed = sample.int(1e3, 1)) {
+  story <- "<p>Ordnen Sie bitte folgende Aussagen/Namen/Begriffe den Wissenschaftstheorien zu. Manche Begriffe spielen bei verschiedenen Wissenschaftstheorien eine Rolle, sind jedoch trotzdem immer einer ganz bestimmten Theorie zuortenbar, da sie dort besonders wichtig sind.</p>
+  <p>
+  <i>Hinweis</i>: Falls Aussagenlogiken (Formeln) vorkommen, steht B für Beobachtung und H für Hypothese.</p>"
+
   set.seed(seed)
   wt <- get_data(wt)
   df <- wt %>%
     #group_by(cols) %>%
     sample_n(5)
-  df2sctable(df, story = "", identifier = "wissenschaftstheorie")
+  df2sctable(df, story = story,
+             identifier = "wissenschaftstheorie")
 }
